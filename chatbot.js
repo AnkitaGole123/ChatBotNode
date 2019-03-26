9b2afa89327bb744b88a58585631a2271274f1a4
sonam kumawat <ankita17@navgurukul.org>
2019-03-26 15:30:03 +0200

    initial commit

---
+++ chatbot.js
@@ -0,0 +1,56 @@
+const express = require('express');
+const app = express()
+const fs = require('fs')
+
+app.use(express.json());
+
+// app.get('/ask_question',(req,res)=>{
+//     fs.readFile('chatbot.json',(err,data)=>{
+        
+        
+        
+//         var jsondata = fs.readFileSync('chatbot.json')
+//         var ressponses = JSON.parse(jsondata);
+        
+//         for(let i = 0; i < ressponses.length; i++){
+            
+//         if(err){
+//             console.log("something went wrong")
+            
+//         }
+//         else if( (user_key  == ressponses[i])){
+
+//             return res.json(ressponses[i])
+            
+//             }
+//         }
+//     })
+// })
+
+app.get('/question  ',(req,res)=>{
+    fs.readFile('information.json',(err,data)=>{
+        if(err){
+            console.log("something went wrong")
+        }
+        else{
+            var data = JSON.parse(data.toString())
+            return res.json(data)
+        }
+    })
+}),
+
+app.get("/navgurukul/:info", (req,res) => {
+    fs.readFile('chatbot.json',(err,data)=>{
+    var body = req.params.info
+    var data1 = JSON.parse(data);
+    var out = data1[body]
+    
+    return res.json(out);
+
+    })
+})
+
+
+
+
+app.listen(3000, () => console.log('server is listening'));
